588b23590ea6cd66c66fe69a44066660f543b0c7
siyan.wang <siyan.wang@cstack.io>
2019-04-04 17:44:06 +0800

    a

--- src/vPlugins/multiCascader/cascader.vue
+++ src/vPlugins/multiCascader/cascader.vue
@@ -1,10 +1,11 @@
 <template>
     <div class="x-cascader" 
-        :style="{left:(level === 0 ? 0 : 1)*width + 'px'}">
+        :style="{left:((level === 0 ? 0 : 1)*(width+3)) + 'px'}">
         <ul class="x-cascader-list" 
             :style="{width: width + 'px', height: height * num +'px'}">
             <li class="x-cascader-item"
                 :style="{width: width + 'px', height: height+'px', 'line-height': height+'px'}"
+                :class="(item.____selected || item.____intermediated) ? 'select':''"
                 v-for="item in data" 
                 :key="item.id" 
                 v-on:click.stop="onItemClick(item);"
@@ -19,16 +20,21 @@
                 <cascader
                     v-if="item.children && item.children.length"
                     v-show="item.____actived"
-                    v-on:on-cascader-intermediate="onIntermediated"
+                    v-on:on-cascader-selected-update="onCascaderSelectedUpdate"
+
+                    v-on:on-cascader-single-parent-intermediated="onSingleParentIntermediate"
+                    v-on:on-cascader-single-parent-selected="onSingleParentSelect"
+
                     :data="item.children"
                     :primary="item.id"
                     :width="width"
                     :height="height"
                     :num="num"
-                    :level="level+1">
+                    :level="level+1"
+                    :type="type">
                 </cascader>
             </li>
-        </ul> 
+        </ul>
     </div>
 </template>
 <script>
@@ -39,31 +45,7 @@
             data:{
                 type: Array,
                 default(){
-                    return [
-                        {name: '1', ____selected:false, ____actived:false, ____intermediated: false, children:[
-                            {name: '1.1',  ____selected:false, ____actived:false, ____intermediated: false, children:[], id: 11},
-                            {name: '1.2',  ____selected:false, ____actived:false, ____intermediated: false, children:[], id: 12},
-                            {name: '1.3',  ____selected:false, ____actived:false, ____intermediated: false, children:[], id: 13},
-                            {name: '1.4',  ____selected:false, ____actived:false, ____intermediated: false, children:[], id: 14},
-                            {name: '1.5',  ____selected:false, ____actived:false, ____intermediated: false, children:[], id: 15}
-                        ], id: 1},
-                        {name: '2',  ____selected:false, ____actived:false, ____intermediated: false, children:[
-                            {name: '2.1',  ____selected:false, ____actived:false, ____intermediated: false, children:[
-                                {name: '2.1.1',  ____selected:false, ____actived:false, ____intermediated: false, children:[], id: 211},
-                                {name: '2.1.2',  ____selected:false, ____actived:false, ____intermediated: false, children:[], id: 212},
-                                {name: '2.1.3',  ____selected:false, ____actived:false, ____intermediated: false, children:[], id: 213},
-                                {name: '2.1.4',  ____selected:false, ____actived:false, ____intermediated: false, children:[], id: 214},
-                                {name: '2.1.5',  ____selected:false, ____actived:false, ____intermediated: false, children:[], id: 215}
-                            ], id: 21},
-                            {name: '2.2',  ____selected:false, ____actived:false, ____intermediated: false, children:[], id: 22},
-                            {name: '2.3',  ____selected:false, ____actived:false, ____intermediated: false, children:[], id: 23},
-                            {name: '2.4',  ____selected:false, ____actived:false, ____intermediated: false, children:[], id: 24},
-                            {name: '2.5',  ____selected:false, ____actived:false, ____intermediated: false, children:[], id: 25}
-                        ], id: 2},
-                        {name: '3',  ____selected:false, ____actived:false, ____intermediated: false, children:[], id: 3},
-                        {name: '4',  ____selected:false, ____actived:false, ____intermediated: false, children:[], id: 4},
-                        {name: '5',  ____selected:false, ____actived:false, ____intermediated: false, children:[], id: 5}
-                    ];
+                    return [];
                 }
             },
             // 选中方式
@@ -73,7 +55,7 @@
             // up           选中子节点会选中父节点，但是选中父节点不会选中子节点
             type:{
                 type: String,
-                default:'down',
+                default:'single',
             },
 
             // 父节点的关键key
@@ -94,7 +76,7 @@
 
             num:{
                 type:[String, Number],
-                default:4,
+                default:7,
             },
 
             level:{
@@ -122,14 +104,19 @@
 
         methods:{
             // 选中
+            // selected 和intermediate互斥
             onItemClick(item){
-                console.log(11111);
+                // 选中 or 不选中
                 item.____selected = !item.____selected;
-                console.log(item.____selected);
+                // 取消半选中
+                item.____intermediated = false;
+                // 根据选中策略进行子节点或者父节点的选中
                 this[this.type](item);
-                console.log(this.type);
-                this.$emit('on-cascader-intermediate', { intermediate: this.intermediate, primary: this.primary });
-                console.log({ intermediate: this.intermediate, primary: this.primary });
+                // 触发父节点选中事件
+                this.$emit('on-cascader-selected-update', {
+                    primary: this.primary,
+                    data: this.data
+                });
             },
 
             // 设置活动的元素（会显示子元素）
@@ -141,19 +128,21 @@
             onItemMouseleave(item){
                 item.____actived = false;
             },
-            // 自定义事件
-            // 设置半选中状态
-            onIntermediated($event){
-                console.log('$event', $event);
-                for(let i =0, len = this.data.length; i < len; i++){
-                    console.log(this.data[i].id, $event.primary);
-                    if( this.data[i].id === $event.primary ){
-                        this.data[i].____intermediated = $event.intermediate;
-                        if($event.intermediate){
-                            this.data[i].____selected = false;
-                        }
+
+            // 根据子节点的数据更新自己的数据
+            onCascaderSelectedUpdate( o ){
+
+                let i = 0, len = this.data.length;
+                for(; i < len; i++ ){
+                    if( this.data[i].id === o.primary ){
+                        this.data[i].children = o.data;
                     }
                 }
+                // 继续执行
+                this.$emit('on-cascader-selected-update', {
+                    primary: this.primary,
+                    data: this.data
+                });
             },
 
             // 元素的操作图标样式
@@ -167,17 +156,97 @@
                 }
             },
 
-            // 选中父节点会选中子节点，选中子节点不会选中父节点
-            down(){
-
+            // 独立策略 父子节点无关系，随意选中
+            single(item){ },
+            // 独立策略 处理同级节点
+            singleSiblings(item){ },
+            // 独立策略 处理父节点
+            singleParents(item){ 
+                item.____selected && this.$emit('on-cascader-single-parent-selected', { 
+                    selected: false, 
+                    primary: this.primary 
+                });
+            },
+            // 独立策略 处理子节点
+            singleChildren(item){
+                if( item.children && item.children.length ){
+                    for( let i =0, len = item.children.length; i< len; i++ ){
+                        item.children[i].____selected = false;
+                        this.singleChildren(item.children[i]);
+                    }
+                }
             },
 
+            // 自定义事件
+            // 独立策略 设置半选中状态事件
+            onSingleParentIntermediate($event){
+                for(let i =0, len = this.data.length; i < len; i++){
+                    if( this.data[i].id === $event.primary ){
+                        // 检测父节点的子节点
+                        if( this.data[i].children && this.data[i].children.length ){
+                            let allI = 0, allS = 0, j = 0, leng = this.data[i].children.length;
+                            for( ; j < leng; j++ ){
+                                // 只要有一个为半选中状态，则父节点也未半选中状态 计数器加1
+                                if( this.data[i].children[j].____intermediated ){
+                                    allI++;
+                                    break;
+                                } 
+                                // 只要子节点为选中状态 计数器加1
+                                if( this.data[i].children[j].____selected ){
+                                    allS++;
+                                }
+                            }
+
+                            if( ( allS !== leng && allS !== 0 ) ||  allI !== 0 ){
+                                this.data[i].____intermediated = true;
+                            }
+                        }
+                        // 如果为半选中， 则选中状态取消
+                        if($event.intermediate){ this.data[i].____selected = false; }
+                    }
+                }
+            },
+
+            // 自定义事件
+            // 独立策略 处理父节点事件
+            onSingleParentSelect($event){
+                for(let i =0, len = this.data.length; i < len; i++){
+                    if( this.data[i].id === $event.primary ){
+                        this.data[i].____selected = $event.selected;
+                        this.$emit('on-cascader-single-parent-selected', { 
+                            selected: false, 
+                            primary: this.primary 
+                        });
+                    }
+                }
+            },
+            
+            // 选中父节点会选中子节点，选中子节点不会选中父节点
+            down(item){
+                this.downParents(item);
+                this.downSiblings(item);
+                this.downChildren(item);
+            },
+            // 向下策略处理同级节点
+            downSiblings(item){ },
+            // 向下策略处理父节点
+            downParents(item){ },
+            // 向下策略处理子节点
+            downChildren(item){
+                if( item.children && item.children.length ){
+                    for( let i =0, len = item.children.length; i< len; i++ ){
+                        item.children[i].____intermediated = false;
+                        item.children[i].____selected = item.____selected;
+                        this.downChildren(item.children[i]);
+                    }
+                }
+            },
+
+            // 向上
             up(){},
 
-            recursive(){},
-
-            // 父子节点无关系
-            single(item){ return; },
+            // 递归
+            recursive(){}
         },
     };
 </script>
@@ -187,22 +256,41 @@
         top: 0;
         .x-cascader-list{
             background-color:#fff;
-            overflow:auto;
+            overflow:hidden;
+            overflow-y:auto;
+            border-right:1px solid #E4E7ED;
+            padding: 8px 1px;
             .x-cascader-item{
                 cursor:pointer;
+                &.select{
+                    display: list-item;
+                    >.x-cascader-item-ele{
+                        color:#1989FA;
+                        font-weight: bold;
+                        &.x-cascader-text{
+                            color:#1989FA;
+                        }
+                    }
+                }
+                &:hover{
+                    background-color: #F2F5FA;
+                }
                 .x-cascader-item-ele{
                     display: inline-block;
                     &.x-cascader-operate{
-                        width: 20px;
+                        width: 14px;
                         font-size:14px;
-                        font-weight: bolder;
+                        margin:0 12px 0 17px;
                     }
                     &.x-cascader-text{
-
+                        color: #3E3E3E;
+                        font-size:14px;
+                        width:130px;
                     }
                     &.x-cascader-children-state{
-                        width: 15px;
+                        width: 10px;
                         font-size:10px;
+                        margin: 0 19px 0 10px;
                     }
                 } 
             }
--- src/vPlugins/multiCascader/index.vue
+++ src/vPlugins/multiCascader/index.vue
@@ -1,8 +1,42 @@
 <template>
     <div class="x-multi-cascader">
-        <div></div>
-        <div class="x-cascader-cont">
-            <cascader></cascader>
+        <div class="x-cascader-shower el-input__inner"
+            tabindex="-1"
+            v-on:blur.stop="onMultiCscaderShowerBlur"
+            v-on:focus.stop="onMultiCscaderShowerFocus">
+            <div class="x-cascader-tips"
+                v-for="item in selects"
+                :key="item.id"
+                :class="item.____active ? 'active' : ''">
+                <span class="x-cascader-title">{{item.name}}</span>
+                <span class="x-cascader-delete el-icon-close" 
+                    v-on:click.stop="onRemoveItem(item)">
+                </span>
+            </div>
+            <input class="x-cascader-input" style="display:none"/>
+            <span class="x-cascader-drop-arrow"
+                :class="isShow ? 'el-icon-arrow-up' : 'el-icon-arrow-down'"    >
+            </span>
+        </div>
+        <div 
+            v-show="isShow"
+            class="x-multi-cascader-cont"
+            tabindex="-1"
+            v-on:blur.stop="onMultiCscaderContBlur"
+            v-on:focus.stop="onMultiCscaderContFocus">
+            <div  class="x-multi-cascader-arrow"></div>
+            <div class="x-multi-cascader-dropdown"
+                :style="{height: height*num + 'px', 'width': max * width + 'px'}">
+                <cascader 
+                    v-on:on-cascader-selected-update="onCascaderSelectedUpdate"
+                    :data="data"
+                    :width="width"
+                    :height="height"
+                    :num="num"
+                    :level="0"
+                    :type="type" >
+                </cascader>
+            </div>
         </div>
     </div>
 </template>
@@ -12,14 +46,222 @@
     export default {
         components:{
             cascader
+        },
+
+        props:{
+            // 选中方式
+            // single       父节点和子节点都能选中，相互之间没关系
+            // recursive    选中父节点时会选中子节点，子节点填满也会选中父节点
+            // down         选中父节点会选中子节点，选中子节点不会选中父节点
+            // up           选中子节点会选中父节点，但是选中父节点不会选中子节点
+            type:{
+                type: String,
+                default:'single',
+            },
+
+            width:{
+                type:[String, Number],
+                default:220,
+            },
+
+            height:{
+                type:[String, Number],
+                default:34,
+            },
+
+            num:{
+                type:[String, Number],
+                default:7,
+            },
+
+            max:{
+                type:[String, Number],
+                default:5,
+            },
+
+                // 数据源
+            data:{
+                type: Array,
+                default(){
+                    return [
+                        {name: '1', ____selected:false, ____actived:false, ____intermediated: false, children:[
+                            {name: '1.1',  ____selected:false, ____actived:false, ____intermediated: false, children:[], id: 11}, 
+                            {name: '1.2',  ____selected:false, ____actived:false, ____intermediated: false, children:[], id: 12},
+                            {name: '1.3',  ____selected:false, ____actived:false, ____intermediated: false, children:[], id: 13},
+                            {name: '1.4',  ____selected:false, ____actived:false, ____intermediated: false, children:[], id: 14},
+                            {name: '1.5',  ____selected:false, ____actived:false, ____intermediated: false, children:[], id: 15}
+                        ], id: 1},
+                        {name: '2',  ____selected:false, ____actived:false, ____intermediated: false, children:[
+                            {name: '2.1',  ____selected:false, ____actived:false, ____intermediated: false, children:[
+                                {name: '2.1.1',  ____selected:false, ____actived:false, ____intermediated: false, children:[], id: 211},
+                                {name: '2.1.2',  ____selected:false, ____actived:false, ____intermediated: false, children:[], id: 212},
+                                {name: '2.1.3',  ____selected:false, ____actived:false, ____intermediated: false, children:[], id: 213},
+                                {name: '2.1.4',  ____selected:false, ____actived:false, ____intermediated: false, children:[], id: 214},
+                                {name: '2.1.5',  ____selected:false, ____actived:false, ____intermediated: false, children:[], id: 215}
+                            ], id: 21},
+                            {name: '2.2',  ____selected:false, ____actived:false, ____intermediated: false, children:[], id: 22},
+                            {name: '2.3',  ____selected:false, ____actived:false, ____intermediated: false, children:[], id: 23},
+                            {name: '2.4',  ____selected:false, ____actived:false, ____intermediated: false, children:[], id: 24},
+                            {name: '2.5',  ____selected:false, ____actived:false, ____intermediated: false, children:[], id: 25}
+                        ], id: 2},
+                        {name: '3',  ____selected:false, ____actived:false, ____intermediated: false, children:[], id: 3},
+                        {name: '4',  ____selected:false, ____actived:false, ____intermediated: false, children:[], id: 4},
+                        {name: '5',  ____selected:false, ____actived:false, ____intermediated: false, children:[], id: 5}
+                    ];
+                }
+            }
+        },
+
+        data(){
+            return {
+                isShowerFocus: false,
+                isContFocus: false
+            };
+        },
+
+        computed:{
+            selects(){
+                return this.select(this.data);
+            },
+
+            isShow(){
+                return this.isShowerFocus || this.isContFocus;
+            }
+        },
+
+        methods:{
+
+            onMultiCscaderShowerBlur(){
+                this.isShowerFocus = false;
+            },
+
+            onMultiCscaderShowerFocus(){
+                this.isShowerFocus = true;
+            },
+
+            onMultiCscaderContBlur(){
+                this.isContFocus = false;
+            },
+            
+            onMultiCscaderContFocus(){
+                this.isContFocus = true;
+            },
+
+            // 根据子节点的数据更新自己的数据
+            onCascaderSelectedUpdate( o ){
+                console.log(o);
+                this.data = o.data;
+            },
+
+            onRemoveItem(item){
+                this.unSelect(item, this.data);
+            },
+
+            unSelect(item, datas){
+                let i = 0, len = datas.length;
+                for(; i < len; i++){
+                    if(datas[i].children && datas[i].children.length){
+                        if(this.unSelect(item, datas[i])){
+                            return true;
+                        };
+                    }
+                    if(datas[i].id === item.id){
+                       datas[i].____selected = false;
+                       return true;
+                    }
+                }
+            },
+
+            select(data){
+                let i = 0, len = data.length, ret =  [];
+                for(; i < len; i++){
+                    if(data[i].____selected){
+                        ret.push(data[i]);
+                    }
+                    if(data[i].children && data[i].children.length){
+                        ret = ret.concat(this.select(data[i].children))
+                    }
+                }
+                return ret;
+            },
         }
     };
 </script>
 <style lang="scss">
     .x-multi-cascader{
         position: relative;
-        .x-cascader-cont{
+        .x-cascader-shower{
+            min-height: 35px;
+            line-height: 35px;
+            height:auto;
             position: relative;
+            .x-cascader-drop-arrow{
+                position: absolute;
+                top: 50%;
+                right: 10px;
+                margin-top: -7px;
+            }
+            .x-cascader-tips{
+                height:25px;
+                line-height: 25px;
+                background:rgba(51,126,255,1);
+                border-radius:2px;
+                border: 1px solid rgba(51,126,255,1);
+                display: inline-block;
+                margin-right:5px;
+                .x-cascader-title{
+                    cursor:pointer;
+                    color:#ffffff;
+                    font-size:12px;
+                    margin:6px 0px 6px 8px;
+                }
+                .x-cascader-delete{
+                    color: #fff;
+                    cursor:pointer;
+                    font-weight: bold;
+                    font-size:12px;
+                    margin:6px;
+                }
+                &.active, &:hover{
+                    background-color:#fff;
+                    .x-cascader-title{
+                        color:rgba(51,126,255,1);
+                    }
+                    .x-cascader-delete{
+                        color:rgba(51,126,255,1);
+                    }
+                }
+            }
+            .x-cascader-input{
+                min-width: 200px;
+            }
+        }
+        .x-multi-cascader-cont{
+            outline:none;
+            width:100%;
+            position: absolute;
+            .x-multi-cascader-arrow{
+                display: inline-block;
+                width: 10px;
+                height: 10px;
+                border: 1px solid rgba(228,231,237,1);
+                background-color:#ffffff;
+                transform: rotate(-45deg);
+                position: absolute;
+                top:15px;
+                left:40px;
+                z-index: 1;
+                border-left-width: 0;
+                border-bottom-width: 0;
+            }
+            .x-multi-cascader-dropdown{
+                position: absolute;
+                box-shadow:2px 2px 5px 0px rgba(0,0,0,0.3);
+                border:1px solid rgba(228,231,237,1);
+                border-radius: 5px;
+                overflow:hidden;
+                top:20px;
+            }
         }
     }
 </style>
